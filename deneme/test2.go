6806afbeb6938bd72a9d468a3eab20355297373b
Bethakin <109422995+Bethakin@users.noreply.github.com>
2025-02-06 14:22:17 +0300

    Update test2.go

--- deneme/test2.go
+++ deneme/test2.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
+	"net/url"
 )
 
-func get_word(
 func main() {
-	// GET endpoint'i tanımla
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Query parametresini al
-		word := r.URL.Query().Get("word")
+	urlStr := "https://example.com/?word=value"
+	myUrl, _ := url.Parse(urlStr)
+	params, _ := url.ParseQuery(myUrl.RawQuery)
+	//fmt.Println(params)
 
-		// Eğer "word" parametresi yoksa hata döndür
-		if word == "" {
-			w.WriteHeader(http.StatusBadRequest) // 400 Bad Request
-			json.NewEncoder(w).Encode(map[string]string{"error": "Lütfen 'word' parametresini ekleyin."})
-			return
-		}
+	word := params.Get("word")
+	fmt.Println(product)
 
-		// JSON objesi oluştur ve döndür
-		response := map[string]string{"word": word}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-	})
-
-	// Sunucuyu başlat
-	fmt.Println("Sunucu http://localhost:8080 adresinde çalışıyor...")
-	http.ListenAndServe(":8080", nil)
 }
